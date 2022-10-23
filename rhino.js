b7605d3751e7f2a36facbcedb440eedd71020c9b
1027603857 <38471268+1027603857@users.noreply.github.com>
2022-10-23 14:04:58 +0800

    Create rhino.js

---
+++ rhino.js
@@ -0,0 +1,12 @@
+var packageName = common.getlpparam().packageName;
+
+if (packageName == 'mark.via'){
+    common.hookMethod('i.a.p.c.a','s',['i.a.p.c.b'],function(param){
+        
+    }, function(param){
+    
+    }, function(param){
+        return;
+    });
+
+}
